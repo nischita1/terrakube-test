a7faf9ca5be5948c16a6a2132e91e0e95e6a035a
nischita1 <66435310+nischita1@users.noreply.github.com>
2026-05-23 00:04:32 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.0"
+}
+
+resource "null_resource" "hello" {
+  provisioner "local-exec" {
+    command = "echo Hello from Terrakube"
+  }
+}
+
+output "message" {
+  value = "Workspace ran successfully"
+}
